ec98ae411fe08ecae24b713ccfaf39da821d71eb
krenartem <krenartem@yandex.ru>
2022-08-29 22:35:43 +0300

    added homework task 041

---
+++ HW_task041/HW_task041.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// ввод чисел
+int[] InputNumbers(int x)
+{
+    int[] arrayNumbers = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        Console.Write($"Введите число {i+1}: ");
+        arrayNumbers[i] = int.Parse(Console.ReadLine());
+    }
+    return arrayNumbers;
+}
+
+// счетчик положительных значений
+void PositiveNumbersCounter(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    Console.WriteLine($"Вы ввели {counter} положительных чисел");
+}
+
+
+Console.Clear();
+Console.WriteLine("Какое количество чисел вы хотите ввести?");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] userArray = InputNumbers(number);
+PositiveNumbersCounter(userArray);
